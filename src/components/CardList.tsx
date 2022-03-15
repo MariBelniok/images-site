efbb32f72d25d18cef0e16bedf8464d6979a865e
maribelniok <mariannabelniok@outlook.com>
2022-03-15 07:39:44 -0300

    card list done

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -16,17 +16,32 @@ interface CardsProps {
 }
 
 export function CardList({ cards }: CardsProps): JSX.Element {
+  const [url, setUrl] = useState('');
   // TODO MODAL USEDISCLOSURE
+  const { onOpen, isOpen, onClose } = useDisclosure();
 
   // TODO SELECTED IMAGE URL STATE
 
   // TODO FUNCTION HANDLE VIEW IMAGE
 
+  const handleViewImage = (urlImage: any): void => {
+    setUrl(urlImage);
+
+    onOpen();
+  };
+
   return (
     <>
       {/* TODO CARD GRID */}
+      <SimpleGrid columnn={3} spacing="40px">
+        {cards &&
+          cards.map(card => (
+            <Card key={card.id} data={card} viewImage={handleViewImage} />
+        ))}
+      </SimpleGrid>
 
       {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage isOpen={isOpen} imgUrl={url} onClose={onClose} />
     </>
   );
 }
